067cac5644de08e8d50f588e3b9621e7f3c8e51e
Derek <derekdperez@gmail.com>
2026-06-11 12:40:42 -0400

    Auto-commit

--- commit.sh
+++ commit.sh
@@ -1,8 +1,12 @@
+echo "Committing changes for agentC..."
 cd ~/repo/agentC
+git pull
 git add .
 git commit -m "Auto-commit"
 git push
+echo "Committing changes for bugbounty..."
 cd ~/repo/agentC/bugbounty
 git add .
 git commit -m "Auto-commit"
 cd ~/repo/agentC
+echo "commit.sh run complete"
